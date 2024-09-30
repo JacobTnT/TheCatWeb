f26cdce3be66e4d7043d216a461160e5ff6093e5
DevInca <antonio17x@hotmail.com>
2024-09-30 17:30:26 -0400

    intial commit

---
+++ function.js
@@ -0,0 +1,40 @@
+console.log('Hello World')
+
+// for of loops
+let draft = '1,2,3,4,5';
+for (let forum of letters){
+    console.log(forum)
+
+ if (draft === 4){
+    break;
+ }
+}
+// Break and conutine
+// for loop
+//let j = '2'
+//for (j < 5){
+
+// if (j === 5){
+ //   break;
+// }
+// console.log(j)
+//}
+
+// while loop way
+
+let j = '3'
+while(j < aa.length){
+
+    if (j === 5){
+        break;
+    }
+ console.log(j)
+ j++;
+}
+const obj = {a: 1, b: 2, c: 3};
+for (let acer in obj){
+    if ( acer === 'n'){
+        break;
+    }
+console.log(obj[acer]);
+}
