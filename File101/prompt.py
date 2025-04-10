abeec741392989e01dfa2dcb13bc3a19eb11e924
DevInca <antonio17x@hotmail.com>
2025-04-10 19:53:17 -0400

    subscribe or else

--- File101/prompt.py
+++ File101/prompt.py
@@ -1,6 +1,12 @@
-def describePet(petName, animalType):
-    print(f"\nI have a {animalType}.")
-    print(f"My {animalType}'s name is {petName.title()}.")
+def lifeSpan(age, country, gender, diet, grandparentAge, smoking):
+ userInput = f"I am {age} from {country}, and I am {gender}. My diet is {diet}, My grandparents ages are {grandparentAge}."
+ print(userInput)
+ return userInput
 
-describePet(animalType='hamster', petName='harry')
-# Compare this snippet from File101/for.py: 
+userInput1 = lifeSpan(5, "USA", "Men", "Protein", "57 and 59", "No")
+userInput2 = lifeSpan(25, "Canada", "Women", "McDonalds", "73 and 71", "Yes I Smoke")
+userInput = userInput1 + userInput2
+if "McDonalds" in userInput:
+ print(f"\nAround 60 years with the McDonalds eating")
+if "Protein" in userInput:
+ print(f"\nAround 70 years for the nice protein")
